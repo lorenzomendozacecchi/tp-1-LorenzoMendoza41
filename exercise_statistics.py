ad67e356fd79bd36798d750c3e1aba4ae4829ff4
lorenzomendozacecchi <lorenzomendozacecchi@gmail.com>
2026-03-07 16:57:01 -0300

    Update exercise_statistics.py
    ejercicio 5 Lorenzo Mendoza

--- exercise_statistics.py
+++ exercise_statistics.py
@@ -12,3 +12,7 @@ def statistics():
     num2 = 8
     num3 = 23
     num4 = 12
+    print((num1 + num2 + num3 + num4) / 4)
+    print(max(num1, num2, num3, num4)
+    print(min(num1, num2, num3 ,num4)
+    print(num3 - num2)    
