8e46484b5a62804774197eb72f1878ba9c279a9e
lorenzomendozacecchi <lorenzomendozacecchi@gmail.com>
2026-03-08 10:45:21 -0300

    Update exercise_age.py
    ejercicio 10 Lorenzo Mendoza

--- exercise_age.py
+++ exercise_age.py
@@ -8,4 +8,13 @@ def age():
     3. La edad en horas (1 día = 24 horas)
     4. La edad en minutos (1 hora = 60 minutos)
     """
-    edad_anos = 25
+    edad_años = 25
+    edad_meses = (edad_años * 12)
+    edad_dias = (edad_años * 365)
+    edad_horas = (edad_dias * 24)
+    edad_minutos = (edad_horas * 60)
+    print(edad_meses)
+    print(edad_dias)
+    print(edad_horas)
+    print(edad_minutos)
+age()
