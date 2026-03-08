4976e5611f95ede214166b8fd68ce4e9f781d320
lorenzomendozacecchi <lorenzomendozacecchi@gmail.com>
2026-03-08 11:16:32 -0300

    Update exercise_currency.py
    ejercicio 13 Lorenzo Mendoza

--- exercise_currency.py
+++ exercise_currency.py
@@ -11,3 +11,10 @@ def currency():
     tasa_dolar = 1500  # 1 dólar = 1500 pesos
     tasa_euro = 1600   # 1 euro = 1600 pesos
     tasa_real = 250    # 1 real = 250 pesos
+    monto_en_dolares = (pesos / tasa_dolar)
+    monto_en_euros = (pesos / tasa_euro)
+    monto_en_reales_brasileños = (pesos / tasa_real)
+    print(monto_en_dolares)
+    print(monto_en_euros)
+    print(monto_en_reales_brasileños)
+currency()
