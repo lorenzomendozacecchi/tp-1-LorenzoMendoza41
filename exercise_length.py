e8c6102642e46d4ed35c8ebb60e77e6895214db1
lorenzomendozacecchi <lorenzomendozacecchi@gmail.com>
2026-03-08 10:14:39 -0300

    Update exercise_length.py
    ejercicio 7 Lorenzo Mendoza

--- exercise_length.py
+++ exercise_length.py
@@ -9,3 +9,12 @@ def length():
     4. Pulgadas (1 pulgada ≈ 0.0254 m)
     """
     metros = 1000
+    kilometros = metros / 1000
+    millas = metros / 1609.34
+    pies = metros / 0.3048
+    pulgadas = metros / 0.0254
+    print(kilometros)
+    print(millas)
+    print(pies)
+    print(pulgadas)
+length()
