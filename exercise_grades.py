0d76e445ac70771973e6c6b94155b202a86e59dc
lorenzomendozacecchi <lorenzomendozacecchi@gmail.com>
2026-03-08 11:01:08 -0300

    Update exercise_grades.py
    ejercicio 11

--- exercise_grades.py
+++ exercise_grades.py
@@ -11,3 +11,12 @@ def grades():
     nota1 = 8
     nota2 = 7
     nota3 = 9
+    promedio = (nota1 + nota2 + nota3) / 3
+    maximo = max(nota1, nota2, nota3)
+    minimo = min(nota1, nota2, nota3)
+    faltante_promedio = 10 - promedio
+    print(promedio)
+    print(maximo)
+    print(minimo)
+    print(faltante_promedio)
+grades()
